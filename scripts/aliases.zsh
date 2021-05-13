115aaf89e72e314b606e455265c734f36e6f516b
Luiz Claudio Moreira Junior <luizclaudiomoreira@gmail.com>
2021-05-13 05:44:16 +0100

    Changes

--- scripts/aliases.zsh
+++ scripts/aliases.zsh
@@ -6,6 +6,7 @@ alias df="df -h"
 alias ls="ls -Glh"
 alias ll="ls -la"
 alias t="tmux"
+alias v="vagrant"
 
 # Rails / Rake / Spring
 alias rk="rake"
@@ -17,4 +18,5 @@ alias sps="clear && spring stop && ps -ax | grep spring"
 alias srk="spring rake"
 alias ssp="clear && spring rspec --format=documentation"
 alias rub="rubocop"
+alias sdbreset="spring rails db:drop && spring rails db:create && spring rails db:migrate"
 alias sdbreset_test="RAILS_ENV=test spring rails db:drop RAILS_ENV=test && RAILS_ENV=test spring rails db:create RAILS_ENV=test && RAILS_ENV=test spring rails db:migrate RAILS_ENV=test"
